5aeb7d202251d585521cd83f99d0fdd422e907cd
snehal <snehal.bargaje@291gmail.com>
2022-04-03 22:05:39 +0530

    changes in dashboard

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import './index.css'
 
 export default function Dashboard() {
   return (
-    <div>
-    <button></button>
-    <button></button>
-    <button></button>
-    <button></button>
-    <button></button>
-    <button></button>
+    <div className='dashboard'>
+    <button className='register'>Register</button>
+    <button className='update'>Update</button>
+    <button className='delete'>Delete</button>
+    <button className='withdraw'>Cash Withdraw</button>
+    <button className='deposit'>Cash Deposit</button>
+    <button className='cheque'>Cheque Deposit</button>
     </div>
   )
 }
