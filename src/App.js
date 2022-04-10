dfbc8dd7b8eca8834ffe1d0bd62329c0e07ac90e
= <=>
2022-04-10 12:01:49 +0530

    added basic registration form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import Dashboard from './components/dashboard';
 import Login from './components/login/index.js';
-import Register from './components/register/index.jsx'; 
+import Register from './components/register/index.jsx';
 
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
-      {/*<Dashboard /> */} 
-      <Register />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -3,18 +3,31 @@ import './index.css'
 
 export default function Register() {
   return (
-    <div>
-      <h1>Registration</h1>
-      <form action="#">
-        <div className="registration-name">
-          <input type="text" name="firstName" id="first-name" placeholder='First Name'/>
-          <input type="text" name="middleName" id="middle-name" placeholder='Middle Name'/>
-          <input type="text" name="lastName" id="last-name" placeholder='Last Name'/>
-        </div>
-
-        <div className="contact-details">
-          <input type="text" name="email" id="emailId" placeholder='Email'/>
-          <input type="tel" name="mobileNumber" id="contact-number" />
+    <div className="register">
+      <form method="post">
+        <div className="register-form-div">
+          <h1>Account Opening</h1>
+          <label htmlFor="account-holder-name">Full Name</label>
+          <input type="text" id="account-holder-name" placeholder='Enter customer name' />
+          <label htmlFor="account-holder-address">Address</label>
+          <textarea name="accountHolderAddress" id="account-holder-address" cols="30" rows="10" placeholder='Enter customer address'></textarea>
+          <label htmlFor="account-holder-phone">Contact Number</label>
+          <input type="number" id="account-holder-phone" placeholder='Enter customer phone number' />
+          <label htmlFor="account-holder-dob">Date of Birth</label>
+          <input type="date"/>
+          <label htmlFor="account-holder-gender">Gender</label>
+          <select id="account-holder-gender" name="accountHolderGender">
+            <option value="MALE">MALE</option>
+            <option value="FEMALE">FEMALE</option>
+            <option value="OTHER">OTHER</option>
+          </select>
+          <label htmlFor="account-holder-type">Account Type</label>
+          <select id="account-holder-type" name="accountHolderType">
+            <option value="SAVING">SAVING</option>
+            <option value="CURRENT">CURRENT</option>
+          </select>
+          <label htmlFor="account-holder-balance">Balance</label>
+          <input id="account-holder-balance" type="number" name="accountHolderBalance" placeholder='Enter customer account balance' defaultValue={0}/>
         </div>
       </form>
     </div>
