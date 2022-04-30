aeb3b388527918e19f537370952eb867c11fe3e0
Harleen Kaur Makhija <hkmakhija26@gmail.com>
2022-04-30 19:27:11 +0530

    deposit component

---
+++ src/components/Delete/Delete.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Delete.css'
+
+function Delete() {
+  return (
+    
+    <form className='delete'>
+      <label>Enter account number:
+        <input type="text" />
+      </label>
+      
+      <input type="Submit" className="submitBtn"/>
+          
+    </form>
+)
+  }
+
+export default Delete~;
